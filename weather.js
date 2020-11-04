96973e937e8939cd4c77d4a30c6bb09eeea07d03
Hosea <hoseasims21@gmail.com>
2020-11-03 23:01:51 -0700

    changed fetch from HTTP to HTTPs

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ const apiKey = "02PzVXhhRMeyZtLKJ9kTXObUiEbuc6Fh";
 // Gets the city data
 
 const getLocation = async (city) => {
-    const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`)
+    const response = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`)
     const data = await response.json();
     console.log(data);
     return data[0];
@@ -16,7 +16,7 @@ const getLocation = async (city) => {
 // Gets the weather data
 
 const getCondition = async (id) => {
-    const response = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiKey}`)
+    const response = await fetch(`https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiKey}`)
     const data = await response.json();
     
     return data[0]
